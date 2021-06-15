784c42a89eba6463f62f03355cd0b452523ba7f2
KA-NZ <kellyann.hubbard@gmail.com>
2021-06-15 20:47:58 +1200

    Chapter 9

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -11,6 +11,8 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
 
+import java.util.concurrent.TimeUnit;
+
 public class BaseTests {
 
     private WebDriver driver;
@@ -20,7 +22,7 @@ public class BaseTests {
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
-
+            // driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         homePage = new HomePage(driver);
     }
 
